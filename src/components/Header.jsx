63c00ffd803b09b92c74ff3b1d8c35abf0e5d479
dxxxn1 <darrenforster3@gmail.com>
2026-07-22 19:28:05 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import china from "../assets/China.jpg";
+
+function Header() {
+  return (
+    <div className="header">
+      <header>
+        <h1>Travel Journal</h1>
+      </header>
+    </div>
+  );
+}
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+import image2 from "../assets/China.jpg";
+function Main() {
+  return (
+    <article>
+      <div>
+        <img src={image2} alt="Chinna" />
+        <div>
+          <a href="https://www.britannica.com/topic/Great-Wall-of-China" />
+          <h1>Great Wall of China</h1>
+          <p>Auguest 2022</p>
+          <p>
+            Great Wall of China, extensive bulwark erected in ancient China, one
+            of the largest building-construction projects ever undertaken. The
+            Great Wall actually consists of numerous walls—many of them parallel
+            to each other—built over some two millennia across northern China
+            and southern Mongolia.
+          </p>
+        </div>
+      </div>
+    </article>
+  );
+}
+export default Main;
